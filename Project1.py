2bde68b7a5631f9d79532a7ef243310cbe1fdf44
SadiaIshtaq <sadiaishtaq21@gmail.com>
2024-02-01 21:36:09 +0530

    Add initial file

--- Project1.py
+++ Project1.py
@@ -1,4 +1,3 @@
-print("We just added a new feature(dropdown).")
-print("We just added a new feature(button).")
-
-
+# Quiz project
+'''In this project we will ask the user say 10 questions and on the basis of the answer score will be given 
++1 for right answer and 0 for wrong answer and then we will display the total score of the users'''
