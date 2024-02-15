b5e24a1ac7bec048f8821530747c47469d0ac3d9
SadiaIshtaq <sadiaishtaq21@gmail.com>
2024-02-15 18:53:13 +0530

    random number generator

--- Project2.py
+++ Project2.py
@@ -1,1 +1,6 @@
-'''In this project we will track how many numbers does the user guess correctly using a random number generator'''
+'''In this project we will track how many numbers does the user guess correctly using a random number generator and giving 1 for correct
+guess and 0 for the wrong guess'''
+
+import random
+num=random.randrange(0,11)
+print(num)
